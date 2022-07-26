f99334c5a33ee5d846562fc66bc6b99cb76409ec
Sonalpt <remsbdj@gmail.com>
2022-07-26 23:08:54 +0200

    added language page and skills page

--- remsb-portfolio/src/pages/Skills.js
+++ remsb-portfolio/src/pages/Skills.js
@@ -3,7 +3,22 @@ import React from "react";
 const Skills = () => {
   return (
     <>
-      <main className="mainContainer"></main>
+      <main className="mainContainer mainSkills">
+        <h2>Web Development</h2>
+        <div className="technologies">
+          <span className="technologies__html">HTML5</span>
+          <span className="technologies__css">CSS3</span>
+          <span className="technologies__js">Javascript</span>
+          <span className="technologies__sass">Sass</span>
+          <span className="technologies__react">React</span>
+          <span className="technologies__node">NodeJS</span>
+          <span className="technologies__mongodb">MongoDB</span>
+          <span className="technologies__mysql">MySQL</span>
+          <span className="technologies__vercel">Vercel</span>
+          <span className="technologies__heroku">Heroku</span>
+          <span className="technologies__git">Git</span>
+        </div>
+      </main>
     </>
   );
 };
